740bfe43ad644337c61522696cce9e7debe9de4b
Joshua <joshua.michael@potawatomi.org>
2022-03-16 10:05:30 -0500

    update submit btn styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				innerBtn: 'inset 2px 4px 2px rgba(0, 0, 0, 0.25)',
+			},
+		},
 	},
 	plugins: [],
 };
