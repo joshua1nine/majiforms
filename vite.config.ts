dd918596994df87800de2262ed9ce69f15cc4ad8
Joshua <joshua.michael@potawatomi.org>
2022-03-22 13:52:31 -0500

    update vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+const path = require('path');
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import mix from 'vite-plugin-mix';
@@ -5,6 +6,16 @@ import vitePluginRequire from 'vite-plugin-require';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/maji-forms/index.ts'),
+			name: 'MajiForms',
+			fileName: (format) => `maji-forms.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+		},
+	},
 	plugins: [
 		react(),
 		mix({
