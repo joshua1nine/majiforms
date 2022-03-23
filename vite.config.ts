29c6caaccac1224f4555e1f73c4ee075f4de2de9
Joshua <joshua.michael@potawatomi.org>
2022-03-23 16:03:03 -0500

    clean up config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import { peerDependencies, dependencies } from './package.json';
+import { peerDependencies } from './package.json';
 import react from '@vitejs/plugin-react';
-// import { VitePluginNode } from 'vite-plugin-node';
-// import vitePluginRequire from 'vite-plugin-require';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,31 +13,17 @@ export default defineConfig({
 			fileName: (format) => `maji-forms.${format}.js`,
 		},
 		rollupOptions: {
-			external: [
-				...Object.keys(peerDependencies),
-				...Object.keys(dependencies),
-			],
+			external: [...Object.keys(peerDependencies)],
 			output: {
 				// Provide global variables to use in the UMD build
 				// for externalized deps
 				globals: {
-					vue: 'React',
+					react: 'React',
 				},
 			},
 		},
 		target: 'esnext',
 		sourcemap: true,
 	},
-	plugins: [
-		react(),
-		// ...VitePluginNode({
-		// 	adapter: 'express',
-		// 	handler: './src/api.ts',
-		// }),
-		// vitePluginRequire({
-		// 	// @fileRegex RegExp
-		// 	// optional：default file processing rules are as follows
-		// 	// fileRegex:/(.jsx?|.tsx?|.vue)$/
-		// }),
-	],
+	plugins: [react()],
 });
